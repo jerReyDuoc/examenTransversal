c26aba34ca98510ad17fe3fa1b302c9ef2821dc2
jerReyDuoc <jers.reyes@duocuc.cl>
2024-07-11 02:54:08 -0400

    avance

--- core/settings.py
+++ core/settings.py
@@ -108,6 +108,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILE_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
